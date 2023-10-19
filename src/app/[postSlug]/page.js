0eff6536a8e9233d8ecff6c437d40a9293b893c8
John Fry <john@sixheads.com>
2023-10-19 14:42:39 +1100

    Create single blog post based on dynamic slug

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { loadBlogPost } from '@/helpers/file-helpers';
 
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
 
-function BlogPost() {
+async function BlogPost({ params }) {
+  // console.log(params);
+  const blogPost = await loadBlogPost(params.postSlug);
+  // could have destructured the above line like so:
+  // const { frontmatter, content } = await loadBlogPost(params.postSlug);
+
+  // console.log(blogPost);
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={blogPost.frontmatter.title}
+        publishedOn={blogPost.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={blogPost.content} />
       </div>
     </article>
   );
